a064921cc7c00e29fa4b59505498c14438412dec
tobon-ets <estobons@gmail.com>
2025-09-04 14:24:29 -0500

    controlador de usuarios

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\UserController;
 
 Route::post('/employees', [EmployeeController::class, 'store']);
 Route::get('/employees', [EmployeeController::class, 'index']);
@@ -12,3 +13,11 @@ Route::put('/employees/document/{document}', [EmployeeController::class, 'update
 
 //to equipments
 Route::post('/equipments', [EquipmentController::class, 'store']);
+
+
+//to users
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/document/{document}', [UserController::class, 'show']);
+Route::delete('/users/document/{document}', [UserController::class, 'destroy']);
+Route::put('/users/document/{document}', [UserController::class, 'update']);
